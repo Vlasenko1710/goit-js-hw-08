cae00db42e4fa6ba9a0f36dd7c072aa25f8fb57f
Vlasenko1710 <vlasenko_ea@ukr.net>
2023-02-26 11:06:34 +0200

    add_gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import '../css/common.css';
+import '../css/01-gallery.css';
+import simpleLightbox from 'simplelightbox';
 // Change code below this line
 
 console.log(galleryItems);
